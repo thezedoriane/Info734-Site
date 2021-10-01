122b32c57596a023ad43241f1b0f3318a85600cf
thezedoriane <72502637+thezedoriane@users.noreply.github.com>
2021-10-01 21:18:42 +0200

    Update todoS.js

--- models/todoS.js
+++ models/todoS.js
@@ -2,36 +2,6 @@ var mongoose = require('mongoose');
 
 var Schema = mongoose.Schema;
 
-var ProductSchema = new Schema({
-  name : String,
-  description : String,
-  price : Float32Array,
-  categoryId : {type:Mongoose.Schema.Types.Object_id,ref:"Category"},
-  done : {
-    type : Boolean,
-    default : false
-  },
-  createdAt : {
-    type : Date,
-    default : Date.now
-  },
-});
-
-var CategorySchema = new Schema({
-    name : String,
-    description : String,
-    shopId : {type:Mongoose.Schema.Types.Object_id,ref:"Shop"},
-    done : {
-      type : Boolean,
-      default : false
-    },
-    createdAt : {
-      type : Date,
-      default : Date.now
-    },
-  });
-
-  
 var ShopSchema = new Schema({
     name : String,
     description : String,
@@ -45,6 +15,4 @@ var ShopSchema = new Schema({
     },
   });
   
-module.exports = mongoose.model('Product', ProductSchema);
-module.exports = mongoose.model('Category', CategorySchema);
 module.exports = mongoose.model('Shop', ShopSchema);
